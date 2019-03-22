33e00f1c81ce96b6734f630fac777dce0a7347d0
Victor Liu <vliu00@stuy.edu>
2019-03-21 20:42:40 -0400

    fixed the resizing (?)

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -6,8 +8,8 @@ public class MyDeque<E>{
   public MyDeque(){
     data = (E[])new Object[10];
     size = 0;
-    start = -1;
-    end = -1;
+    start = 0;
+    end = 0;
   }
 
   public MyDeque(int initialCapacity){
@@ -19,7 +21,7 @@ public class MyDeque<E>{
   }
 
   public String toString(){
-    if (start == -1 || end == -1){
+    if (size == 0){
       return "{}";
     }
     String ans = "{";
@@ -31,7 +33,7 @@ public class MyDeque<E>{
       }
     } else {
       while (incrementStart != end+1){
-        //System.out.println("incrementStart: "+incrementStart+", end: "+end);
+        //this allows the list to loop back on itself
         ans+= data[incrementStart] + " ";
         if (incrementStart == data.length){
           incrementStart = 0;
@@ -46,46 +48,56 @@ public class MyDeque<E>{
 
 
   public void addFirst(E element){
-    size++;
-    if (start == -1){
-      start = 0;
-      end = 0;
-    } else {
+    if (element == null){
+      throw new NullPointerException();
+    }
+    if (size != 0){
+      //if you reached the front end by doing start--'ing
       if (start == 0){
+        //if there's empty space at the end...
         if (data[end] == null){
           start = data.length-1;
         } else {
-          //resize
+          resize();
+          start = data.length-1;
         }
       } else {
         start--;
       }
     }
+    size++;
     data[start] = element;
   }
 
   public void addLast(E element){
-    size++;
+    if (element == null){
+      throw new NullPointerException();
+    }
     if (end == data.length - 1){
+      //if there's empty space in the beginning
       if (data[0] == null){
         end = 0;
       } else {
-        //resize
+        resize();
+        end++;
       }
     } else {
-      if (start == -1){
-        start = 0;
+      if (size != 0){
+        end++; //if we're just adding for the first time, then end should not increment away from start
       }
-      end++;
     }
+    size++;
     data[end] = element;
   }
 
   public E removeFirst(){
+    if (size == 0){
+      throw new NoSuchElementException();
+    }
     E storer = data[start];
     if (start != data.length - 1){
       start++;
-    } else {
+    } else { //if you need to loop back to the beginning
       start = 0;
     }
     size--;
@@ -93,10 +105,13 @@ public class MyDeque<E>{
   }
 
   public E removeLast(){
+    if (size == 0){
+      throw new NoSuchElementException();
+    }
     E storer = data[end];
     if (end != 0){
       end--;
-    } else {
+    } else { //if you need to loop back to the end
       end = data.length;
     }
     size--;
@@ -104,13 +119,28 @@ public class MyDeque<E>{
   }
 
   public E getFirst(){
+    if (size == 0){
+      throw new NoSuchElementException();
+    }
     return data[start];
   }
 
   public E getLast(){
+    if (size == 0){
+      throw new NoSuchElementException();
+    }
     return data[end];
   }
 
+  private void resize(){
+    E[] newAry = (E[])new Object[data.length * 2 + 1];
+    for (int n = 0; n < data.length; n++){
+        newAry[n] = data[n];
+    }
+    data = newAry;
+  }
+
+  //for debugging purposes
   public String toStringActual(){
     String ans = "{";
     for (int i = 0; i < data.length; i++){
@@ -125,6 +155,4 @@ public class MyDeque<E>{
 Add: (push / en-queue)
 Get: return but NOT remove the element. (peek)
 Remove: return AND remove the element. (pop / de-queue)
-
-Size,Add, remove, and get should be O(1)
 */
--- VDriver.java
+++ VDriver.java
@@ -1,7 +1,62 @@
+import java.util.NoSuchElementException;
+
 public class VDriver{
   public static void main(String[]args){
     MyDeque<Integer> d1 = new MyDeque<>();
-    //d1.addLast(1); d1.addLast(2); d1.addLast(3);
-    System.out.println(d1);
+    System.out.print(d1+" "); System.out.println(d1.size());//{}, 0
+    d1.addLast(1); d1.addLast(2); d1.addLast(3); //{1 2 3 }, 3
+    System.out.print(d1+" "); System.out.println(d1.size());
+    d1.removeFirst(); //{2 3 }, 2
+    System.out.print(d1+" "); System.out.println(d1.size());
+    d1.removeLast(); //{2 }, 1
+    System.out.print(d1+" "); System.out.println(d1.size());
+    d1.removeFirst(); //{}, 0
+    System.out.print(d1+" "); System.out.println(d1.size());
+    int exceptions = 0;
+    try {
+      d1.removeFirst();
+    } catch (NoSuchElementException e){
+      exceptions++;
+    }
+
+    try {
+      d1.removeLast();
+    } catch (NoSuchElementException e){
+      exceptions++;
+    }
+
+    try {
+      d1.getFirst();
+    } catch (NoSuchElementException e){
+      exceptions++;
+    }
+
+    try {
+      d1.getLast();
+    } catch (NoSuchElementException e){
+      exceptions++;
+    }
+
+    try {
+      d1.addFirst(null);
+    } catch (NullPointerException e){
+      exceptions++;
+    }
+
+    try {
+      d1.addLast(null);
+    } catch (NullPointerException e){
+      exceptions++;
+    }
+
+    if (exceptions == 6){
+      System.out.println("SUCCESS");
+    }
   }
+  //tested all exceptions
+
+  //need to test resizing, getFirst, getLast, removeLast (for looping back),
+  //removeFirst (for looping back), addFirst (for looping back and resizing),
+  //addLast (for looping back and resizing), toString for when the list is looped
+  //size when list is looped, and instantiating MyDeque with initialcapicity constructor
 }
