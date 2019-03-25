3397ced16c359072374663d3e56c44a23a9dbaf5
Victor Liu <vliu00@stuy.edu>
2019-03-25 18:01:43 -0400

    included division and mod test cases

--- VDriver.java
+++ VDriver.java
@@ -6,7 +6,8 @@ public class VDriver{
     System.out.println(Calculator.eval("10 2.0 +"));// is 12.0
     System.out.println(Calculator.eval("11 3 - 4 + 2.5 *"));// is 30.0
     System.out.println(Calculator.eval("8 2 + 99 9 - * 2 + 9 -"));// is 893.0
-    System.out.println(Calculator.eval("1 2 3 4 5 + * - -"));// is 26.0
+    System.out.println(Calculator.eval("1 2 / 3 4 + * 5 +"));// is 8.5
+    System.out.println(Calculator.eval("3 14 3 % + 5 %"));// is 0.0
 
 
     /*
