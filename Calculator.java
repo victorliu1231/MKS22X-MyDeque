0626d7e708185bd9f63ebc70b18f0282ce4d7e0b
Victor Liu <vliu00@stuy.edu>
2019-03-25 17:52:40 -0400

    started calculator part of lab

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      String[] elements = s.split(" ");
+      String ops = "+-*/%";
+      MyDeque<Double> deq = new MyDeque<>(elements.length);
+      for (int i = 0; i < elements.length; i++){
+        if (ops.contains(elements[i])){
+          double prev = deq.removeLast();
+          double prevPrev = deq.removeLast();
+          if (elements[i].equals("+")){
+            deq.addLast(prev + prevPrev);
+          } else if (elements[i].equals("-")){
+            deq.addLast(prev - prevPrev);
+          } else if (elements[i].equals("*")){
+            deq.addLast(prev * prevPrev);
+          } else if (elements[i].equals("/")){
+            deq.addLast(prev / prevPrev);
+          } else {
+            deq.addLast(prev % prevPrev);
+          }
+        } else {
+          deq.addLast(Double.parseDouble(elements[i]));
+        }
+      }
+      return deq.removeLast();
+    }
+}
